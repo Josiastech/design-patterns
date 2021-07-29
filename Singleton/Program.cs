75b9b3022517f3f55eef4337b3bff8668572d53f
Josias <josiastech@gmail.com>
2021-07-29 10:57:03 -0600

    Add first design patter example: singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instace.mensaje);
+            Singleton.Instace.mensaje = "Hola marte";
+            Console.WriteLine(Singleton.Instace.mensaje);
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+
+        protected Singleton()
+        {
+            mensaje = "hola mundo";
+        }
+
+        public static Singleton Instace
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
